c4c5a2ce09ad7fa3d6c7c0b764a5840ed0d95d0f
Pierre Talbot <ptalbot@hyc.io>
2014-02-05 19:55:00 +0100

    Don't create duplicate schema anymore, change schema name to the email address.

--- mail_extractor/mail_extractor.py
+++ mail_extractor/mail_extractor.py
@@ -8,6 +8,19 @@ import textwrap
 from email.header import decode_header
 from email.parser import BytesParser
 
+class Person:
+  def __init__(self, name, email):
+    self.email = email
+    self.name = name
+
+  def to_schema(self):
+    schema = """\
+    <div itemscope itemtype="http://schema.org/Person">
+      <span itemprop="name">%s</span>
+      <span itemprop="email">%s</span>
+    </div>""" % (self.name, self.email)
+    return textwrap.dedent(schema)
+
 def make_utf8_str(s):
   res = ""
   for (data, encoding) in decode_header(s):
@@ -17,33 +30,40 @@ def make_utf8_str(s):
       res += data
   return res
 
-def guess_from_mail_addr(mailAddr):
+def guess_name_from_mail_addr(mailAddr):
   return mailAddr.partition('@')[0]
 
-def make_person_schema(mailFile, schemaFile):
+def merge_person(p1, p2):
+  return p2
+
+def make_person_schema(mailFile, outputDir, person_db):
   msg = BytesParser().parse(mailFile)
   (realname, mailAddr) = email.utils.parseaddr(msg['from'])
-  realname = make_utf8_str(realname)
   mailAddr = make_utf8_str(mailAddr)
+  realname = make_utf8_str(realname)
   if not realname:
-    realname = guess_from_mail_addr(mailAddr)
-  schema = """\
-  <div itemscope itemtype="http://schema.org/Person">
-    <span itemprop="name">%s</span>
-    <span itemprop="email">%s</span>
-  </div>""" % (realname, mailAddr)
-  schemaFile.write(textwrap.dedent(schema))
+    realname = guess_name_from_mail_addr(mailAddr)
+  person = Person(realname, mailAddr)
+
+  if mailAddr in person_db:
+    person_db[mailAddr] = merge_person(person, person_db[mailAddr])
+  else:
+    person_db[mailAddr] = person
+
+  person = person_db[mailAddr]
+  # Write the new person schema to file.
+  schemaFilename = "%s/%s.html" % (outputDir, mailAddr)
+  with open(schemaFilename, 'w', encoding='utf8') as schemaFile:
+    schemaFile.write(person.to_schema())
 
 def mails2schema(mailDir, outputDir):
-  i = 0
+  person_db = {}
   for mail in os.listdir(mailDir):
     mailFilename = mailDir + "/" + mail
     print(mailFilename)
     if(os.path.isfile(mailFilename)):
-      schemaFilename = "%s/person%d.html" % (outputDir,i)
-      i += 1
-      with open(mailFilename, 'r+b') as mailFile, open(schemaFilename, 'w', encoding='utf8') as schemaFile:
-        make_person_schema(mailFile, schemaFile)
+      with open(mailFilename, 'r+b') as mailFile:
+        make_person_schema(mailFile, outputDir, person_db)
 
 def main():
   parser = argparse.ArgumentParser(description='Mail to schema')
